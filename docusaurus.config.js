1f2478761ba459fd6db31209b3e61c8278970f34
Jaaayden <z862956872@gmail.com>
2023-04-05 00:14:47 +0800

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Jaaayden/Jaaayden.github.io/tree/main/docs/',
+            'https://github.com/Jaaayden/Jaaayden.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Jaaayden/Jaaayden.github.io/tree/main/blog/',
+            'https://github.com/Jaaayden/Jaaayden.github.io/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
