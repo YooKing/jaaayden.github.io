e245300078b1eea4642d0b2f79afa5697bbdc7be
Jaaayden <z862956872@gmail.com>
2023-04-08 01:35:59 +0800

    comment

---
+++ blog/2023-04-07-Time to Comment.md
@@ -0,0 +1,13 @@
+---
+slug: Time to Comment
+title: Time to Comment
+authors: Jayden
+enableComments: true # for Gisqus
+tags: [Photo, Delicacy]
+---
+
+The **comment** function of this website is opened. 
+
+It's enormous progress for this website. You can talk to me in the comments easily instead of sending me an email if you have a GitHub account.
+
+**Try it out in the comments below！**
---
+++ src/components/GiscusComponent.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Giscus from "@giscus/react";
+import { useColorMode } from '@docusaurus/theme-common';
+
+export default function GiscusComponent() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Giscus    
+      repo="Jaaayden/discussion.Jaaayden.github.io"
+      repoId="R_kgDOJT9xrQ"
+      category="General"
+      categoryId="DIC_kwDOJT9xrc4CVnZV"  // E.g. id of "General"
+      mapping="url"                        // Important! To map comments to URL
+      term="Welcome to @giscus/react component!"
+      strict="0"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light"
+      lang="en"
+      // loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  );
+}
+{/* <script src="https://giscus.app/client.js"
+        data-repo="Jaaayden/discussion.Jaaayden.github.io"
+        data-repo-id="R_kgDOJT9xrQ"
+        data-category="General"
+        data-category-id="DIC_kwDOJT9xrc4CVnZV"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="light"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script> */}
